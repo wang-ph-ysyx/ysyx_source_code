e348df2c3c9fc1556ef1bf5471005d303a44534c
tracer-ysyx <tracer@ysyx.org>
2024-02-25 23:00:10 +0800

    > compile NEMU ysyx_do not have one 王子卿 Linux WP 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:00:10 up 1 day, 12:10,  9 users,  load average: 0.13, 0.24, 0.28

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -78,6 +78,7 @@ static int cmd_x(char *args) {
 	uint8_t* haddr = NULL;
 	paddr_t paddr = 0;
 	sscanf(args, "%d %x", &num, &paddr);
+	if(paddr) return 0;
 	haddr = guest_to_host(paddr);
 	for (; num > 0; --num) {
 		printf("0x%x:  %x %x %x %x\n", paddr, *haddr, *(haddr+1), *(haddr+2), *(haddr+3));
