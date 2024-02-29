c04faf044c5fbcfd91dca64959d0e03cca3cdf9c
tracer-ysyx <tracer@ysyx.org>
2024-02-29 12:00:52 +0800

    > compile NEMU ysyx_do not have one 王子卿 Linux WP 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:00:52 up  2:08,  4 users,  load average: 0.18, 0.23, 0.43

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -135,7 +135,7 @@ word_t expr(char *e, bool *success) {
   }
 	int i = 0;
 	for (; i < nr_token; ++i) {
-		Log("%c %s", tokens[i].type, tokens[i].str);
+		Log("%d %s", tokens[i].type, tokens[i].str);
 	}
 	return eval(0, nr_token-1, success);
 }
