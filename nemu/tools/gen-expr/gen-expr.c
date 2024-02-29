a7d0e6df3afdf31bbf8b85a8551456f5623748c2
tracer-ysyx <tracer@ysyx.org>
2024-02-29 17:44:56 +0800

    > compile NEMU ysyx_do not have one 王子卿 Linux WP 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:44:56 up  7:52,  8 users,  load average: 0.33, 0.34, 0.34

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -105,6 +105,7 @@ int main(int argc, char *argv[]) {
     uint32_t result = 0;
     ret = fscanf(fp, "%u", &result);
     pclose(fp);
+		if (fp != 0) continue;
 
     printf("%u %s\n", result, buf);
   }
