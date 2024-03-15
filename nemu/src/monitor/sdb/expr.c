596aec37363ebc7ce9e65f39255a6d0659b2e00d
tracer-ysyx <tracer@ysyx.org>
2024-03-15 20:12:00 +0800

    > compile NEMU ysyx_do not have one 王子卿 Linux WP 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:12:00 up  4:27,  3 users,  load average: 0.17, 0.18, 0.25

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -201,7 +201,7 @@ word_t eval(int p, int q, bool *success) {
 					*success = false;
 					return 0;
 				}				
-				return val1 / val2;
+				return (sword_t)val1 / (sword_t)val2;
 			case TK_EQ: return val1 == val2;
 			case TK_UNEQ: return val1 != val2;
 			case TK_AND: return val1 && val2;
