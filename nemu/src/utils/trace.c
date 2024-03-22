bf6900a97add41d3f68312d02c3f2648de333b79
tracer-ysyx <tracer@ysyx.org>
2024-03-22 20:04:44 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:04:44 up  4:23,  7 users,  load average: 0.16, 0.17, 0.13

--- nemu/src/utils/trace.c
+++ nemu/src/utils/trace.c
@@ -2,28 +2,20 @@
 
 #define RINGBUF_SIZE 10
 
-char ringbuf[RINGBUF_SIZE][128];
+typedef struct ringbuf_node{
+	vaddr_t pc, snpc;
+	uint8_t *inst;
+} ringbuf_node;
+ringbuf_node ringbuf[RINGBUF_SIZE];
 int start = 0, end = 0;
 
 void record_ringbuf(vaddr_t pc, vaddr_t snpc, uint8_t *inst) {
 	end = (end + 1) % RINGBUF_SIZE;
 	if (start == end) 
 		start = (start + 1) % RINGBUF_SIZE;
-	char *p = ringbuf[end];
-	p += snprintf(p, sizeof(ringbuf[end]), FMT_WORD ":", pc);
-	int ilen = snpc - pc;
-	for (int i = ilen - 1; i >= 0; i--)
-		p += snprintf(p, 4, " %02x", inst[i]);
-	int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
-	int space_len = ilen_max - ilen;
-	if (space_len < 0) space_len = 0;
-	space_len = space_len * 3 + 1;
-	memset(p, ' ', space_len);
-	p += space_len;
-
-	void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
-	disassemble(p, ringbuf[end] + sizeof(ringbuf[end]) - p,
-			MUXDEF(CONFIG_ISA_x86, snpc, pc), inst, ilen);
+	ringbuf[end].pc = pc;
+	ringbuf[end].snpc = snpc;
+	ringbuf[end].inst = inst;
 }
 
 void display_ringbuf(){
@@ -31,6 +23,25 @@ void display_ringbuf(){
 		start = (start + 1) % RINGBUF_SIZE;
 		if (start == end) 
 			printf("--> ");
-		printf("    %s\n", ringbuf[start]);
+		vaddr_t pc = ringbuf[end].pc;
+		vaddr_t snpc = ringbuf[end].snpc;
+		uint8_t *inst = ringbuf[end].inst;
+		char buf[128] = {'\0'};
+		char *p = buf;
+		p += snprintf(p, sizeof(buf), FMT_WORD ":", pc);
+		int ilen = snpc - pc;
+		for (int i = ilen - 1; i >= 0; i--)
+			p += snprintf(p, 4, " %02x", inst[i]);
+		int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
+		int space_len = ilen_max - ilen;
+		if (space_len < 0) space_len = 0;
+		space_len = space_len * 3 + 1;
+		memset(p, ' ', space_len);
+		p += space_len;
+	
+		void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
+		disassemble(p, buf + sizeof(buf) - p,
+			MUXDEF(CONFIG_ISA_x86, snpc, pc), inst, ilen);
+		printf("    %s\n", buf);
 	}
 }
