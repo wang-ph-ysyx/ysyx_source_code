162489066a3bfba57fd2fb9eca758c0f890349dc
tracer-ysyx <tracer@ysyx.org>
2024-03-23 19:57:04 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:57:04 up  9:15,  7 users,  load average: 0.17, 0.14, 0.19

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -83,7 +83,7 @@ static void execute(uint64_t n) {
     if (nemu_state.state != NEMU_RUNNING) break;
     IFDEF(CONFIG_DEVICE, device_update());
   }
-	panic("test cpu-exec.c line 86");
+	//panic("test cpu-exec.c line 86");
 }
 
 static void statistic() {
