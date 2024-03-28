64ec21c608ca3de290789fc105e339f1440dc5b4
tracer-ysyx <tracer@ysyx.org>
2024-03-28 20:07:51 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:07:51 up  9:45,  7 users,  load average: 0.16, 0.41, 0.34

--- nemu/src/utils/trace.c
+++ nemu/src/utils/trace.c
@@ -126,8 +126,8 @@ static int nest = 0;
 void ftrace_call(vaddr_t addr) {
 	for (int i = 0; i < tail; ++i) {
 		if (addr >= symtab[i].st_value && addr < symtab[i].st_value + symtab[i].st_size) {
-			//for (int j = 0; j < nest; ++j)
-				//printf(" ");
+			for (int j = 0; j < nest; ++j)
+				printf(" ");
 			++nest;
 			printf("call [%s@%#lx]\n", names[i], symtab[i].st_value);
 			break;
