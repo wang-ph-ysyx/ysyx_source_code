cbbeb47de295cff6b02116844cfc1caf9c666b84
tracer-ysyx <tracer@ysyx.org>
2024-04-03 21:30:35 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:30:35 up  9:42, 10 users,  load average: 0.38, 0.36, 0.27

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -9,7 +9,7 @@ void __am_gpu_init() {
 	int h = 300;
 	uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
 	for (i = 0; i < w * h; i ++) fb[i] = i;
-	outl(SYNC_ADDR, 0);
+	outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
