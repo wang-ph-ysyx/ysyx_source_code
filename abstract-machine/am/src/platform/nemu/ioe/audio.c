7b214ce25524cf532c0a0cb6a457aed202979dd0
tracer-ysyx <tracer@ysyx.org>
2024-04-04 18:11:07 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:11:07 up  8:49,  8 users,  load average: 0.23, 0.32, 0.38

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -12,15 +12,33 @@ void __am_audio_init() {
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = false;
+  cfg->present = true;
+	cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
+	outl(AUDIO_FREQ_ADDR, ctrl->freq);
+	outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
+	outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
+	outl(AUDIO_INIT_ADDR, 1);
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = 0;
+  stat->count = inl(AUDIO_COUNT_ADDR);
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+	int bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
+
+	//wait until the space is big enough
+	while (ctl->buf.end - ctl->buf.start > bufsize - inl(AUDIO_COUNT_ADDR));
+
+	uint8_t *fb = (uint8_t *)(uintptr_t)AUDIO_SBUF_ADDR;
+	static int i = 0;
+	uint8_t *st = (uint8_t *)ctl->buf.start, *ed = (uint8_t *)ctl->buf.end;
+	for (uint8_t *start = st; start < ed; ++start, i = (i + 1) % bufsize) {
+		fb[i] = *start;
+	}
+	int count = inl(AUDIO_COUNT_ADDR);
+	outl(AUDIO_COUNT_ADDR, count + ed - st);
 }
--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -29,8 +29,29 @@ enum {
 
 static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
+static uint8_t *sbuf_front = NULL;
+
+void mycallback(void *userdata, uint8_t *stream, int len) {
+	stream = sbuf_front;
+	sbuf_front += len;
+	audio_base[reg_count] -= len;
+}
 
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
+	if (is_write && offset == 16 && audio_base[reg_init] == 1) {
+		SDL_AudioSpec s = {};
+		s.format = AUDIO_S16SYS;
+		s.userdata = NULL;
+		
+		s.freq = audio_base[reg_freq];
+		s.channels = audio_base[reg_channels];
+		s.samples = audio_base[reg_samples];
+		s.callback = mycallback;
+  
+		SDL_InitSubSystem(SDL_INIT_AUDIO);
+		SDL_OpenAudio(&s, NULL);
+		SDL_PauseAudio(0);
+	}
 }
 
 void init_audio() {
@@ -44,4 +65,5 @@ void init_audio() {
 
   sbuf = (uint8_t *)new_space(CONFIG_SB_SIZE);
   add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, NULL);
+	sbuf_front = sbuf;
 }
