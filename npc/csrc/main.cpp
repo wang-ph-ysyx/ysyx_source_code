8b851967d1566111120b6340a6ffd308748326d6
tracer-ysyx <tracer@ysyx.org>
2024-04-05 21:08:58 +0800

    > sim RTL ysyx_23060236 王子卿 Linux WP 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:08:58 up 10:36,  7 users,  load average: 0.18, 0.20, 0.33

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,10 +1,13 @@
 #include <Vtop.h>
 #include "verilated.h"
 #include <stdint.h>
+#include <stdio.h>
+#include <assert.h>
 
 #define MEM_BASE 0x80000000
+#define MEM_SIZE 0x8000000
 
-uint8_t memory[10000];
+static uint8_t memory[MEM_SIZE];
 
 void pmem_write(uint32_t addr, uint32_t data) {
 	uint32_t index = addr - MEM_BASE;
@@ -16,6 +19,34 @@ unsigned pmem_read(uint32_t addr) {
 	return *(uint32_t *)(memory + index);
 }
 
+void load_img(char *img_file) {
+	if (img_file == NULL) {
+		printf("No image is given. Use the default build-in image.");
+		return;
+	}
+
+	FILE *fp = fopen(img_file, "rb");
+	assert(fp);
+
+	fseek(fp, 0, SEEK_END);
+	long size = ftell(fp);
+
+	fseek(fp, 0, SEEK_SET);
+	int ret = fread(memory, size, 1, fp);
+	assert(ret == 1);
+
+	fclose(fp);
+}
+
+void init_memory() {
+	pmem_write(0x80000000, 0x00100093);
+	pmem_write(0x80000004, 0x00108113);
+	pmem_write(0x80000008, 0x00210093);
+	pmem_write(0x8000000c, 0x00310113);
+	pmem_write(0x80000010, 0x00408093);
+	pmem_write(0x80000014, 0x00100073);
+}
+
 void one_cycle(Vtop* top) {
 	top->clk = 0; top->eval();
 	top->clk = 1; top->eval();
@@ -25,12 +56,11 @@ int main(int argc, char **argv) {
 	VerilatedContext* contextp = new VerilatedContext;
 	contextp->commandArgs(argc, argv);
 	Vtop* top = new Vtop{contextp};
-	pmem_write(0x80000000, 0x00100093);
-	pmem_write(0x80000004, 0x00108113);
-	pmem_write(0x80000008, 0x00210093);
-	pmem_write(0x8000000c, 0x00310113);
-	pmem_write(0x80000010, 0x00408093);
-	pmem_write(0x80000014, 0x00100073);
+
+	init_memory();
+	char *img_file = argc > 0 ? argv[1] : NULL;
+	load_img(img_file);
+
 	top->reset = 1;
 	one_cycle(top);
 	top->reset = 0;
