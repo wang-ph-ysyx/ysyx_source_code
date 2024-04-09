054684cd12dce97a934f522924f34eb50c35bb5c
tracer-ysyx <tracer@ysyx.org>
2024-04-09 23:24:19 +0800

    > sim RTL ysyx_23060236 王子卿 Linux WP 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:24:19 up  5:02,  1 user,  load average: 0.10, 0.48, 0.76

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -37,7 +37,7 @@ int main(int argc, char **argv) {
 
 	if (top->halt_ret)
 		printf("\33[1;31mHIT BAD TRAP\33[1;0m\n");
-	else printf("\33[1;32mHIT GOOD TREP\33[1;0m\n");
+	else printf("\33[1;32mHIT GOOD TRAP\33[1;0m\n");
 
 	delete top;
 	delete contextp;
