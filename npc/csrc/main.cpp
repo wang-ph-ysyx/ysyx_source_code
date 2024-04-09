995f49b1b9a3319d5687cc5367700be26abee908
tracer-ysyx <tracer@ysyx.org>
2024-04-09 21:29:21 +0800

    > sim RTL ysyx_23060236 王子卿 Linux WP 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:29:21 up  3:07,  9 users,  load average: 0.23, 0.26, 0.28

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -5,30 +5,38 @@
 #include <assert.h>
 #include <memory.h>
 
-void init_monitor(int argc, char **argv);
+Vtop* top = NULL;
 
-void one_cycle(Vtop* top) {
+void init_monitor(int argc, char **argv);
+void sdb_mainloop();
+
+int one_cycle() {
+	top->inst = pmem_read(top->pc);
 	top->clk = 0; top->eval();
 	top->clk = 1; top->eval();
+	return top->finished;
+}
+
+void reset() {
+	top->reset = 1;
+	top->clk = 0; top->eval();
+	top->clk = 1; top->eval();
+	top->reset = 0;
 }
 
 int main(int argc, char **argv) {
 	VerilatedContext* contextp = new VerilatedContext;
 	contextp->commandArgs(argc, argv);
-	Vtop* top = new Vtop{contextp};
+	top = new Vtop{contextp};
 
 	init_monitor(argc, argv);
 
-	top->reset = 1;
-	one_cycle(top);
-	top->reset = 0;
-	while(!top->finished) {
-		top->inst = pmem_read(top->pc);
-		one_cycle(top);
-	}
+	sdb_mainloop();
+
 	if (top->halt_ret)
 		printf("\33[1;31mHIT BAD TRAP\33[1;0m\n");
 	else printf("\33[1;32mHIT GOOD TREP\33[1;0m\n");
+
 	delete top;
 	delete contextp;
 	return 0;
