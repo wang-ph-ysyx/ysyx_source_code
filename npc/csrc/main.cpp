ff413c9c031617207fbcd4f41f160832aa3aaae7
tracer-ysyx <tracer@ysyx.org>
2024-04-10 19:25:26 +0800

    > sim RTL ysyx_23060236 王子卿 Linux WP 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:25:26 up  7:37,  7 users,  load average: 0.05, 0.36, 0.34

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -27,6 +27,7 @@ void cpu_exec(unsigned n) {
 		if (top->finished) break;
 	}
 
+	if (!top->finished) return;
 	if (top->halt_ret)
 		printf("\33[1;31mHIT BAD TRAP\33[1;0m ");
 	else printf("\33[1;32mHIT GOOD TRAP\33[1;0m ");
