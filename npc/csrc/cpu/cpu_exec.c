c373e79fe81ac4902174c6ce45669c32f75a5f25
tracer-ysyx <tracer@ysyx.org>
2024-04-11 17:03:30 +0800

    > sim RTL ysyx_23060236 王子卿 Linux WP 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:03:30 up  7:10,  4 users,  load average: 0.30, 0.18, 0.17

---
+++ npc/csrc/cpu/cpu_exec.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <Vtop.h>
+#include "verilated.h"
+#include <memory.h>
+
+Vtop *top = NULL;
+
+void one_cycle() {
+	top->inst = pmem_read(top->pc);
+	top->clk = 0; top->eval();
+	top->clk = 1; top->eval();
+}
+
+void cpu_exec(unsigned n) {
+	if (top->finished) {
+		printf("the program is ended.\n");
+		return;
+	}
+
+	for (; n > 0; --n) {
+		one_cycle();
+		if (top->finished) break;
+	}
+
+	if (!top->finished) return;
+	if (top->halt_ret)
+		printf("\33[1;31mHIT BAD TRAP\33[1;0m ");
+	else printf("\33[1;32mHIT GOOD TRAP\33[1;0m ");
+	printf("at pc = %#x\n", top->pc);
+}
+
+void reset() {
+	top->reset = 1;
+	top->clk = 0; top->eval();
+	top->clk = 1; top->eval();
+	top->reset = 0;
+}
