1fb6b3707cb08e0ec3dbe0d900e54ecb312141ed
tracer-ysyx <tracer@ysyx.org>
2024-04-18 14:03:32 +0800

    > sim RTL ysyx_23060236 王子卿 Linux WP 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:03:32 up  1:47,  7 users,  load average: 0.16, 0.11, 0.04

--- npc/vsrc/exu.v
+++ npc/vsrc/exu.v
@@ -1,6 +1,3 @@
-import "DPI-C" function int pmem_read(input int raddr);
-import "DPI-C" function void pmem_write(
-	  input int waddr, input int wdata, input byte wmask);
 module exu(
 	input [6:0] opcode,
 	input [31:0] src1,
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,3 +1,6 @@
+import "DPI-C" function int pmem_read(input int raddr);
+import "DPI-C" function void pmem_write(
+	  input int waddr, input int wdata, input byte wmask);
 module top(
 	input clk,
 	input reset,
