409004b4170de828186923ffe52facd62a6caa3c
tracer-ysyx <tracer@ysyx.org>
2024-04-19 17:30:47 +0800

    > sim RTL ysyx_23060236 王子卿 Linux WP 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:30:47 up  6:08,  7 users,  load average: 0.11, 0.29, 0.29

--- npc/vsrc/exu.v
+++ npc/vsrc/exu.v
@@ -44,7 +44,7 @@ module exu(
 		})
 	);
 
-	MuxKeyInternal #(8, 10, 32, 1) calculate_val1(
+	MuxKeyInternal #(9, 10, 32, 1) calculate_val1(
 		.out(val1),
 		.key({funct3, opcode}),
 		.default_out(32'b0),
@@ -52,6 +52,7 @@ module exu(
 			10'b0010000011, (rdata & 32'hffff) | {{16{rdata[15]}}, 16'h0}, //lh
 			10'b0100000011, rdata,                  //lw
 			10'b1000000011, rdata & 32'hff,         //lbu
+			10'b1010000011, rdata & 32'hffff,       //lhu
 			10'b0000010011, src1 + imm,             //addi
 			10'b0110010011, {31'b0, {src1 < imm}},  //sltiu
 			10'b1000010011, src1 ^ imm,             //xori
