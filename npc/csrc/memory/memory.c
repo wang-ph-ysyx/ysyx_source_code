60d087e12e413cd9cca322dafed1d4ba46c2203d
tracer-ysyx <tracer@ysyx.org>
2024-04-19 18:57:27 +0800

    > sim RTL ysyx_23060236 王子卿 Linux WP 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:57:27 up  7:35,  5 users,  load average: 0.08, 0.08, 0.09

--- npc/csrc/memory/memory.c
+++ npc/csrc/memory/memory.c
@@ -1,17 +1,24 @@
 #include <memory.h>
 #include <stdio.h>
 
+#define SERIAL 0xa00003f8
+
 static uint8_t memory[MEM_SIZE];
 
 uint8_t *guest2host(uint32_t paddr) {return memory + paddr - MEM_BASE;}
 uint32_t host2guest(uint8_t *haddr) {return haddr - memory + MEM_BASE;}
 
 extern "C" void pmem_write(int waddr, int wdata, char wmask) {
+	if (!(wmask & 0x1)) wdata = wdata & ~0xff;
+	if (!(wmask & 0x2)) wdata = wdata & ~0xff00;
+	if (!(wmask & 0x4)) wdata = wdata & ~0xff0000;
+	if (!(wmask & 0x8)) wdata = wdata & ~0xff000000;
+	if (waddr == SERIAL) {
+		putchar(wdata);
+		return;
+	}
 	uint8_t *haddr = guest2host(waddr/* & ~0x3u*/);
-	if (wmask & 0x1) haddr[0] = wdata & 0xff;
-	if (wmask & 0x2) haddr[1] = (wdata >> 8) & 0xff;
-	if (wmask & 0x4) haddr[2] = (wdata >> 16) & 0xff;
-	if (wmask & 0x8) haddr[3] = (wdata >> 24) & 0xff;
+	*(uint32_t *)haddr = wdata;
 }
 
 extern "C" int pmem_read(int raddr) {
