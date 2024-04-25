c78aa450faf2e27d345f219a710de8e9ccfbc31b
tracer-ysyx <tracer@ysyx.org>
2024-04-25 15:55:55 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:55:55 up  6:06, 11 users,  load average: 0.23, 0.31, 0.49

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -32,6 +32,9 @@ static void restart() {
 
   /* The zero register is always 0. */
   cpu.gpr[0] = 0;
+
+	/* To support difftest, the mstatus rigister is initially 0x1800*/
+	cpu.csr.mstatus = 0x1800;
 }
 
 void init_isa() {
