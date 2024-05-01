6ccc39d452a8725966a863ce9ab57da5dba7f3fc
tracer-ysyx <tracer@ysyx.org>
2024-05-02 00:09:49 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:09:49 up 12:40,  7 users,  load average: 0.03, 0.15, 0.21

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -39,7 +39,8 @@ static int num2str(int data, char *str, int base, int format, int sign) {
 		num /= base;
 		++i;
 	}
-	if (i >= 0) --i;
+	if (i > 0) --i;
+	else buf[0] = '0';
 	for (; i >= 0; --i, ++len) {
 		str[len] = buf[i];
 	}
