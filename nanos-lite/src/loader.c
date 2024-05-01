36a0522eef9000fe1180df52015270c6a16be340
tracer-ysyx <tracer@ysyx.org>
2024-05-01 20:08:13 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:08:13 up  8:39,  7 users,  load average: 0.01, 0.12, 0.18

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,6 +9,16 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+#if defined(__ISA_AM_NATIVE__)
+# define EXPECT_TYPE EM_X86_64
+#elif defined(__ISA_X86__)
+# define EXPECT_TYPE EM_X86_64
+#elif defined(__ISA_RISCV32__)
+# define EXPECT_TYPR EM_RISCV
+#else
+# error Unsupported ISA
+#endif
+
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
