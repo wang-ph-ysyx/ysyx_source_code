003305222375fa59b3944d46380b9bc0ed1c0be3
tracer-ysyx <tracer@ysyx.org>
2024-05-02 13:32:50 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:32:50 up  2:57,  3 users,  load average: 0.21, 0.26, 0.57

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,11 +1,15 @@
 #include <common.h>
 #include "syscall.h"
+#include <stdio.h>
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 	a[1] = c->GPR2;
 	a[2] = c->GPR3;
 	a[3] = c->GPR4;
+
+	printf("cause: %d, args: %d %d %d\n", a[0], a[1], a[2], a[3]);
 
   switch (a[0]) {
 		case 0: halt(0);
