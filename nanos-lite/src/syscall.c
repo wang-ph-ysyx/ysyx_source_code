2b926061ab930d5a2d4316cf41552bf0f9fdd0e5
tracer-ysyx <tracer@ysyx.org>
2024-05-02 13:40:57 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:40:57 up  3:06,  4 users,  load average: 0.18, 0.22, 0.42

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -13,7 +13,7 @@ void do_syscall(Context *c) {
 
   switch (a[0]) {
 		case SYS_exit: halt(a[1]);
-		case 1: yield(); c->GPRx = 0; break;
+		case SYS_yield: yield(); c->GPRx = 0; break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
