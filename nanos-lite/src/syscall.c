dc9d72ad948397ffc171e5b2e03f530df3f5d92c
tracer-ysyx <tracer@ysyx.org>
2024-05-03 15:40:41 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:40:41 up  4:44,  6 users,  load average: 0.04, 0.05, 0.09

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,6 +2,12 @@
 #include "syscall.h"
 #include <stdio.h>
 
+int fs_open(const char *pathname, int flags, int mode);
+size_t fs_read(int fd, void *buf, size_t len);
+size_t fs_write(int fd, const void *buf, size_t len);
+size_t fs_lseek(int fd, size_t offset, int whence);
+int fs_close(int fd);
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -15,14 +21,19 @@ void do_syscall(Context *c) {
   switch (a[0]) {
 		case SYS_exit: halt(a[1]); break;
 		case SYS_yield: yield(); c->GPRx = 0; break;
+		case SYS_open: c->GPRx = fs_open((char *)a[1], a[2], a[3]); break;
+		case SYS_read: c->GPRx = fs_read(a[1], (char *)a[2], a[3]); break;
 		case SYS_write: 
 			if (a[1] == 1 || a[1] == 2) {
 				for (int i = 0; i < a[3]; ++i) {
 					putch(*((uint8_t *)a[2] + i));
 				}
+				c->GPRx = a[3];
 			}
-			c->GPRx = a[3];
+			else c->GPRx = fs_write(a[1], (char *)a[2], a[3]);
 			break;
+		case SYS_close: c->GPRx = fs_close(a[1]); break;
+		case SYS_lseek: c->GPRx = fs_lseek(a[1], a[2], a[3]); break;
 		case SYS_brk: c->GPRx = 0; break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
