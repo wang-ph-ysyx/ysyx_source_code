3b773bff0a1c322cbab584a3e44a4429781b63d0
tracer-ysyx <tracer@ysyx.org>
2024-05-03 17:35:49 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:35:49 up  6:39,  9 users,  load average: 0.15, 0.24, 0.28

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -51,17 +51,21 @@ int fs_open(const char *pathname, int flags, int mode) {
 }
 
 size_t fs_read(int fd, void *buf, size_t len) {
-	assert(file_table[fd].open_offset + len <= file_table[fd].size);
-	ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
-	file_table[fd].open_offset += len;
-	return len;
+	size_t read_len = len;
+	if (read_len + file_table[fd].open_offset > file_table[fd].size)
+		read_len = file_table[fd].size - file_table[fd].open_offset;
+	ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, read_len);
+	file_table[fd].open_offset += read_len;
+	return read_len;
 }
 
 size_t fs_write(int fd, const void *buf, size_t len) {
-	assert(file_table[fd].open_offset + len <= file_table[fd].size);
-	ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
-	file_table[fd].open_offset += len;
-	return len;
+	size_t write_len = len;
+	if (write_len + file_table[fd].open_offset > file_table[fd].size)
+		write_len = file_table[fd].size - file_table[fd].open_offset;
+	ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, write_len);
+	file_table[fd].open_offset += write_len;
+	return write_len;
 }
 
 size_t fs_lseek(int fd, size_t offset, int whence) {
