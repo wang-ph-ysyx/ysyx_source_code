a6d1a87c0fdf56a29f64fec14fa941c9ef65d55c
tracer-ysyx <tracer@ysyx.org>
2024-05-03 23:52:56 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:52:56 up 12:56,  8 users,  load average: 0.14, 0.15, 0.22

--- navy-apps/tests/timer-test/timer.c
+++ navy-apps/tests/timer-test/timer.c
@@ -1,25 +1,19 @@
 #include <sys/time.h>
 #include <stdio.h>
+#include <NDL.h>
 
-#define HALF_SECOND 500000
+#define HALF_SECOND 500
 #define SECOND (2 * HALF_SECOND)
 
 int main() {
-	struct timeval tv;
-	struct timezone tz;
-
 	printf("clock start\n");
-	gettimeofday(&tv, &tz);
-	printf("%ld, %ld, %d, %d\n", tv.tv_sec, tv.tv_usec, tz.tz_minuteswest, tz.tz_dsttime);
 
+	uint32_t start = NDL_GetTicks();
+	uint32_t interval = 0;
 	int half_sec = 1;
-	time_t start = tv.tv_sec;
-	suseconds_t ustart = tv.tv_usec;
-	suseconds_t interval = 0;
   while (1) {
 		while(interval / HALF_SECOND < half_sec) {
-			gettimeofday(&tv, &tz);
-			interval = (tv.tv_sec - start) * SECOND + tv.tv_usec - ustart;
+			interval = NDL_GetTicks() - start;
 		}
 		printf("%d half_sec\n", half_sec ++);
 	}
