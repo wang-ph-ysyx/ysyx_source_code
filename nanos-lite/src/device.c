ff9e07d4d74f8255446889cace23e4b6e921a122
tracer-ysyx <tracer@ysyx.org>
2024-05-04 20:46:23 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:46:23 up  9:18,  9 users,  load average: 0.15, 0.26, 0.27

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -52,10 +52,9 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 size_t fb_write(const void *buf, size_t offset, size_t len) {
 	AM_GPU_CONFIG_T gpu = io_read(AM_GPU_CONFIG);
 	int width = gpu.width;
-	printf("%d\n", width);
 	int x = offset / 4 % width;
 	int y = offset / 4 / width;
-	for (int i = 0; i < len / 4; ++i) {
+	for (int i = 0; i < len ; ++i) {
 		io_write(AM_GPU_FBDRAW, x, y, buf + 4 * i, 1, 1, 1);
 		offset += 4;
 		x = offset / 4 % width;
