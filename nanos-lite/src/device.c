696b867961680543adde5eb36e2f36a662253433
tracer-ysyx <tracer@ysyx.org>
2024-05-04 21:30:22 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:30:22 up 10:02,  9 users,  load average: 0.20, 0.20, 0.27

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -55,7 +55,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
 	int x = offset / 4 % width;
 	int y = offset / 4 / width;
 	for (int i = 0; i < len / 4; ++i) {
-		io_write(AM_GPU_FBDRAW, x, y, buf +  i, 1, 1, 1);
+		io_write(AM_GPU_FBDRAW, x, y, buf + 4 * i, 1, 1, 1);
 		offset += 4;
 		x = offset / 4 % width;
 		y = offset / 4 / width;
