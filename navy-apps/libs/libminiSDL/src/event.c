ba2f43ad3310bc545ec3ede699156e122df24833
tracer-ysyx <tracer@ysyx.org>
2024-05-05 16:32:32 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:32:32 up  5:21,  7 users,  load average: 0.28, 0.18, 0.17

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,5 +1,6 @@
 #include <NDL.h>
 #include <SDL.h>
+#include <string.h>
 
 #define keyname(k) #k,
 
@@ -17,6 +18,17 @@ int SDL_PollEvent(SDL_Event *ev) {
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
+	if (event == NULL) return 1;
+	char buf[32];
+	if (NDL_PollEvent(buf, sizeof(buf)) == 0) return 1;
+	if (buf[1] == 'u') event->type = SDL_KEYUP;
+	else if (buf[1] == 'd') event->type = SDL_KEYDOWN;
+	for (int i = 1; i < sizeof(keyname) / sizeof(char *); ++i) {
+		if (strcmp(buf + 3, keyname[i]) == 0) {
+			event->key.keysym.sym = i;
+			return 1;
+		}
+	}
   return 1;
 }
 
