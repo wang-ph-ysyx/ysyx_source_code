3556982bfc64e607b0000b2bf3cd186d84c0cc6c
tracer-ysyx <tracer@ysyx.org>
2024-05-05 19:20:18 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:20:18 up  8:09,  7 users,  load average: 0.17, 0.19, 0.31

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -140,7 +140,6 @@ static void draw_text_row(char *s, int r) {
 }
 
 static void display_menu(int n) {
-	printf("test\n");
   clear_display();
   SDL_Rect rect = { .x = screen->w - logo_sf->w, .y = 0 };
   SDL_BlitSurface(logo_sf, NULL, screen, &rect);
