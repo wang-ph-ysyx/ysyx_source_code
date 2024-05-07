0f5477d97735f13f3143a59c802f2650d4ae3594
tracer-ysyx <tracer@ysyx.org>
2024-05-07 23:29:42 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:29:42 up 1 day, 13:03,  7 users,  load average: 0.09, 0.11, 0.16

--- navy-apps/libs/libam/src/ioe.c
+++ navy-apps/libs/libam/src/ioe.c
@@ -1,8 +1,69 @@
 #include <am.h>
 
+#define NAME(key) \
+  [AM_KEY_##key] = #key
+
+static const char *keyname[256] __attribute__((used)) = {
+	[AM_KEY_NONE] = "NONE",
+	AM_KEYS(NAME)
+};
+
+void __am_timer_config(AM_TIMER_CONFIG_T *cfg) {
+	cfg->present = true; cfg->has_rtc = true;
+}
+
+void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
+	rtc->year = 1900;
+	rtc->month = rtc->day = rtc-hour = rtc->minute = rtc->second = 0;
+}
+
+void __am_timer_uptime(AM_TIMER_RTC_T *uptime) {
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	uptime->us = tv.tv_sec * 1000000 + tv.tv_usec;
+}
+
+void __am_input_config(AM_INPUT_CONFIG_T *cfg) {
+	cfg->present = true;
+}
+
+void __am_input_keybrd(AM_INPUT_KEYBRD *kbd) {
+	int fd = open("/dev/events", 0, 0);
+	char buf[32];
+	if (read(fd, buf, sizeof(buf)) == 0) {
+		kbd->keycode = 0;
+		close(fd);
+		return;
+	}
+	close(fd);
+	if (buf[1] == 'd')
+		kbd->keydown = true;
+	else if (buf[1] == 'u') 
+		kbd->keydown = false;
+	for (int i = 1; i < sizeof(keyname) / sizeof(char *); ++i) {
+		if (strcmp(keyname[i], buf[3]) == 0) {
+			kbd->keycode = i;
+			return;
+		}
+	}
+}
+
+typedef void (*handler_t)(void *buf);
+static void *lut[128] = {
+	[AM_TIMER_CONFIG] = __am_timer_config,
+	[AM_TIMER_RTC   ] = __am_timer_rtc,
+	[AM_TIMER_UPTIME] = __am_timer_uptime,
+	[AM_INPUT_CONFIG] = __am_input_config,
+	[AM_INPUT_KEYBRD] = __am_input_keybrd,
+};
+
+static void fail(void *buf) { panic("access nonexist register"); }
+
 bool ioe_init() {
+	for (int i = 0; i < LENGTH(lut); i++)
+		if (!lut[i]) lut[i] = fail;
   return true;
 }
 
-void ioe_read (int reg, void *buf) { }
-void ioe_write(int reg, void *buf) { }
+void ioe_read (int reg, void *buf) { ((handler_t)lut[reg])(buf); }
+void ioe_write(int reg, void *buf) { ((handler_t)lut[reg])(buf); }
