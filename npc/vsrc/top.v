95129aa8d8c34d970f19c8c34e9da5ce50c95f66
tracer-ysyx <tracer@ysyx.org>
2024-05-09 22:09:57 +0800

    > sim RTL ysyx_23060236 王子卿 Linux WP 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:09:57 up 1 day, 10:25,  4 users,  load average: 0.34, 0.39, 0.25

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -7,7 +7,7 @@ module top(
 	output [31:0] pc,
 	output finished,
 	output [31:0] halt_ret,
-	output wb_valid
+	output reg ifu_valid
 );
 
 	reg [31:0] inst;
@@ -36,8 +36,8 @@ module top(
 	wire lsu_wen;
 	wire lsu_ren;
 	wire lsu_valid;
-	reg ifu_valid;
 	wire idu_valid;
+	wire wb_valid;
 	wire [7:0] wmask;
 
 	parameter TYPE_R = 3'd0,  TYPE_I = 3'd1, TYPE_S = 3'd2, TYPE_B = 3'd3, TYPE_U = 3'd4, TYPE_J = 3'd5;
