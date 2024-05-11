f3f16b21583cc018b4b670c7ce78dd9f0d905acf
tracer-ysyx <tracer@ysyx.org>
2024-05-11 15:31:51 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:31:51 up  6:04,  6 users,  load average: 0.21, 0.25, 0.36

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -57,6 +57,7 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
 		for (int i = 0; i < size; ++i) {
 			uint8_t index = *((uint8_t *)s->pixels + i);
 			SDL_Color *color = s->format->palette->colors + index;
+			printf("%d\n", index);
 			pixels[i] = 0;
 			pixels[i] |= (uint32_t)color->a << 24;
 			pixels[i] |= (uint32_t)color->r << 16;
