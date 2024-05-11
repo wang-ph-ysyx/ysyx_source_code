a22c8cb9aacc76d4817f1b944924b0970af328a9
tracer-ysyx <tracer@ysyx.org>
2024-05-11 14:21:29 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:21:29 up  4:54,  5 users,  load average: 0.47, 0.46, 0.77

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -52,7 +52,7 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
 		NDL_DrawRect((uint32_t *)s->pixels, x, y, w, h);
 	else if (s->format->BitsPerPixel == 8) {
 		int size = w * h;
-		printf("%d\n", size);
+		printf("%d, %d\n", w, h);
 		uint32_t *pixels = malloc(size * sizeof(uint32_t));
 		assert(pixels);
 		for (int i = 0; i < size; ++i) {
