f2fea551ec58e7d57771a5ca1daf82ad6a42d0c0
tracer-ysyx <tracer@ysyx.org>
2024-05-13 18:18:37 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:18:37 up  7:09,  9 users,  load average: 0.82, 0.71, 0.45

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -27,7 +27,8 @@ void context_kload(PCB *_pcb, void (*entry)(void *), void *arg) {
 }
 
 void init_proc() {
-	context_kload(&pcb[0], hello_fun, (void *)1L);
+	//context_kload(&pcb[0], hello_fun, (void *)1L);
+	context_uload(&pcb[0], "/bin/hello");
 	context_uload(&pcb[1], "/bin/pal");
   switch_boot_pcb();
 
