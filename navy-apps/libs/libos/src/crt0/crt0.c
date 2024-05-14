7eb3ea1012f10f1a41ece337b6b4970ef851d0dc
tracer-ysyx <tracer@ysyx.org>
2024-05-14 12:15:53 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:15:53 up  2:14,  3 users,  load average: 0.58, 0.40, 0.61

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -5,12 +5,12 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-	argc = *(int *)args;
+	int argc = *(int *)args;
 	args = (uintptr_t *)((int *)args + 1);
-	argv = (char **)args;
-	args = (uintptr_t)((char **)args + argc + 1);
-	envp = (char **)envp;
+	char **argv = (char **)args;
+	args = (uintptr_t *)((char **)args + argc + 1);
+	char **envp = (char **)args;
   environ = envp;
-  exit(main(0, empty, empty));
+  exit(main(argc, argv, envp));
   assert(0);
 }
