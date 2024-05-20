c5c1e3f3d6b74e8c769b533af89b6c832ae74b5c
tracer-ysyx <tracer@ysyx.org>
2024-05-20 23:20:39 +0800

    > sim RTL ysyx_23060236 王子卿 Linux WP 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:20:39 up 12:37,  7 users,  load average: 0.20, 0.21, 0.18

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -44,6 +44,19 @@ module top(
 	wire ifu_rvalid;
 	wire [1:0] ifu_rresp;
 	wire ifu_rready;
+	wire lsu_arvalid;
+	wire lsu_arready;
+	wire [31:0] lsu_rdata;
+	wire [1:0] lsu_rresp;
+	wire lsu_rvalid;
+	wire lsu_rready;
+	wire lsu_awvalid;
+	wire lsu_awready;
+	wire lsu_wvalid;
+	wire lsu_wready;
+	wire [1:0] lsu_bresp;
+	wire lsu_bvalid;
+	wire lsu_bready;
 
 	parameter TYPE_R = 3'd0,  TYPE_I = 3'd1, TYPE_S = 3'd2, TYPE_B = 3'd3, TYPE_U = 3'd4, TYPE_J = 3'd5;
 
@@ -123,18 +136,39 @@ module top(
 		.wmask(wmask)
 	);
 
-	lsu my_lsu(
+	MuxKeyInternal #(5, 10, 32, 1) caculate_lsu_val(
+		.out(lsu_val),
+		.key({funct3, opcode}),
+		.default_out(32'b0),
+		.lut({
+			10'b0000000011, (lsu_rdata & 32'hff) | {{24{lsu_rdata[7]}}, 8'h0},     //lb
+			10'b0010000011, (lsu_rdata & 32'hffff) | {{16{lsu_rdata[15]}}, 16'h0}, //lh
+			10'b0100000011, lsu_rdata,                                             //lw
+			10'b1000000011, lsu_rdata & 32'hff,                                    //lbu
+			10'b1010000011, lsu_rdata & 32'hffff                                   //lhu
+		})
+	);
+
+	sram lsu_sram(
 		.clk(clk),
-		.raddr(src1 + imm),
-		.ren(lsu_ren),
-		.val(lsu_val),
-		.waddr(src1 + imm),
+		.reset(reset),
+		.araddr(src1 + imm),
+		.arvalid(lsu_arvalid),
+		.arready(lsu_arready),
+		.rdata(lsu_rdata),
+		.rresp(lsu_rresp),
+		.rvalid(lsu_rvalid),
+		.rready(lsu_rready),
+		.awaddr(src1 + imm),
+		.awvalid(lsu_awvalid),
+		.awready(lsu_awready),
 		.wdata(src2),
-		.wen(lsu_wen),
-		.wmask(wmask),
-		.valid(lsu_valid),
-		.opcode(opcode),
-		.funct3(funct3)
+		.wstrb(wmask[3:0]),
+		.wvalid(lsu_wvalid),
+		.wready(lsu_wready),
+		.bresp(lsu_bresp),
+		.bvalid(lsu_bvalid),
+		.bready(lsu_bready)
 	);
 
 	RegisterFile #(5, 32) my_reg(
@@ -172,11 +206,44 @@ module top(
 	assign val = (exu_val | csr_val | lsu_val);
 	assign csr_enable = (opcode == 7'b1110011) & (funct3 != 3'b000);
 	assign jump = exu_jump | csr_jump;
-	assign wb_valid = ~ifu_arvalid & (~lsu_ren | lsu_valid);
 	assign ifu_rready = 1;
+	assign lsu_rready = 1;
+	assign lsu_bready = 1;
 	assign idu_valid = ifu_rvalid & ifu_rready;
 
-	Reg #(1, 0) reg_ifu_arvalid(
+	Reg #(1, 0) reg_lsu_arvalid(
+		.clk(clk),
+		.rst(reset),
+		.din(lsu_arvalid & ~lsu_arready | ~lsu_arvalid & lsu_ren),
+		.dout(lsu_arvalid),
+		.wen(1)
+	);
+
+	Reg #(1, 0) reg_lsu_awvalid(
+		.clk(clk),
+		.rst(reset),
+		.din(lsu_awvalid & ~lsu_awready | ~lsu_awvalid & lsu_wen),
+		.dout(lsu_awvalid),
+		.wen(1)
+	);
+
+	Reg #(1, 0) reg_lsu_wvalid(
+		.clk(clk),
+		.rst(reset),
+		.din(lsu_wvalid & ~lsu_wready | ~lsu_wvalid & lsu_wen),
+		.dout(lsu_wvalid),
+		.wen(1)
+	);
+
+	Reg #(1, 0) reg_wb_valid(
+		.clk(clk),
+		.rst(reset),
+		.din(~wb_valid & (lsu_rvalid & lsu_rready | lsu_bvalid & lsu_bready | idu_valid & (opcode != 7'b0000011) & (opcode != 7'b0100011))),
+		.dout(wb_valid),
+		.wen(1)
+	);
+
+	Reg #(1, 1) reg_ifu_arvalid(
 		.clk(clk),
 		.rst(reset),
 		.din(ifu_arvalid & ~ifu_arready | ~ifu_arvalid & wb_valid),
