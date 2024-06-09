fcf0543d9513937b9e1c7c0205adfe34357ebecc
tracer-ysyx <tracer@ysyx.org>
2024-06-09 18:41:15 +0800

    > sim RTL ysyx_23060236 王子卿 Linux WP 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:41:15 up  7:52,  2 users,  load average: 0.15, 0.18, 0.18

--- npc/vsrc/ysyx_23060236_xbar.v
+++ npc/vsrc/ysyx_23060236_xbar.v
@@ -105,25 +105,25 @@ module ysyx_23060236_xbar(
 
 	assign araddr = lsu_araddr;
 
-	assign ifu_arready   = ifu_reading & io_master_arready;
-	assign lsu_arready   = lsu_reading & (soc_reading & io_master_arready | clint_reading & clint_arready);
-	assign io_master_arvalid  = ifu_reading & ifu_arvalid | lsu_reading & soc_reading & lsu_arvalid;
-	assign clint_arvalid = lsu_reading & clint_reading & lsu_arvalid;
-	assign io_master_araddr   = {32{ifu_reading}} & ifu_araddr | {32{lsu_reading}} & {32{soc_reading}} & lsu_araddr;
-	assign clint_araddr  = {32{lsu_reading}} & {32{clint_reading}} & lsu_araddr;
-	assign io_master_arid = 0;
-	assign io_master_arlen = 0;
-	assign io_master_arsize = {3{ifu_reading}} & 3'b010 | {3{lsu_reading}} & lsu_arsize;
+	assign ifu_arready       = ifu_reading & io_master_arready;
+	assign lsu_arready       = lsu_reading & (soc_reading & io_master_arready | clint_reading & clint_arready);
+	assign io_master_arvalid = ifu_reading & ifu_arvalid | lsu_reading & soc_reading & lsu_arvalid;
+	assign clint_arvalid     = lsu_reading & clint_reading & lsu_arvalid;
+	assign io_master_araddr  = {32{ifu_reading}} & ifu_araddr | {32{lsu_reading}} & {32{soc_reading}} & lsu_araddr;
+	assign clint_araddr      = {32{lsu_reading}} & {32{clint_reading}} & lsu_araddr;
+	assign io_master_arid    = 0;
+	assign io_master_arlen   = 0;
+	assign io_master_arsize  = {3{ifu_reading}} & 3'b010 | {3{lsu_reading}} & lsu_arsize;
 	assign io_master_arburst = 0;
 
 	assign io_master_rready  = ifu_reading & ifu_rready | lsu_reading & lsu_rready & soc_reading;
-	assign clint_rready = lsu_reading & clint_reading & lsu_rready;
-	assign ifu_rvalid   = ifu_reading & io_master_rvalid;
-	assign lsu_rvalid   = lsu_reading & (soc_reading & io_master_rvalid | clint_reading & clint_rvalid);
-	assign ifu_rresp    = {2{ifu_reading}} & io_master_rresp;
-	assign lsu_rresp    = {2{lsu_reading}} & ({2{soc_reading}} & io_master_rresp | {2{clint_reading}} & clint_rresp);
-	assign ifu_rdata    = {32{ifu_reading}} & io_master_rdata[31:0];
-	assign lsu_rdata    = {32{lsu_reading}} & ({32{soc_reading}} & io_master_rdata[31:0] | {32{clint_reading}} & clint_rdata);
+	assign clint_rready      = lsu_reading & clint_reading & lsu_rready;
+	assign ifu_rvalid        = ifu_reading & io_master_rvalid;
+	assign lsu_rvalid        = lsu_reading & (soc_reading & io_master_rvalid | clint_reading & clint_rvalid);
+	assign ifu_rresp         = {2{ifu_reading}} & io_master_rresp;
+	assign lsu_rresp         = {2{lsu_reading}} & ({2{soc_reading}} & io_master_rresp | {2{clint_reading}} & clint_rresp);
+	assign ifu_rdata         = {32{ifu_reading}} & io_master_rdata[31:0];
+	assign lsu_rdata         = {32{lsu_reading}} & ({32{soc_reading}} & io_master_rdata[31:0] | {32{clint_reading}} & clint_rdata);
 
 	assign lsu_awready       = io_master_awready;
 	assign io_master_awvalid = lsu_awvalid;
