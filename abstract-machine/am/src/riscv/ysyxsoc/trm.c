883792109698dfd23a3d54917f0912ccdc245e78
王子卿 <wang_ph@sjtu.edu.cn>
2024-06-09 17:07:44 +0800

    实现最简单的soc,为ysyxSoC添加AM运行时环境

---
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -0,0 +1,28 @@
+#include <am.h>
+#include <klib-macros.h>
+
+#define SERIAL_PORT 0x10000000
+static inline void outb(uintptr_t addr, uint8_t data) { *(volatile uint8_t *)addr = data; }
+
+int main(const char *args);
+
+Area heap = RANGE(0x0f000000, 0x0f001fff);
+#ifndef MAINARGS
+#define MAINARGS ""
+#endif
+static const char mainargs[] = MAINARGS;
+
+void putch(char ch) {
+	outb(SERIAL_PORT, ch);
+}
+
+void halt(int code) {
+	asm volatile ("mv a0,%0; ebreak" : :"r"(code));
+
+	while(1);
+}
+
+void _trm_init() {
+	int ret = main(mainargs);
+	halt(ret);
+}
--- npc/include/memory.h
+++ npc/include/memory.h
@@ -3,8 +3,9 @@
 
 #include <stdint.h>
 
-#define MEM_BASE 0x80000000
-#define MEM_SIZE 0x8000000
+#define MEM_BASE  0x80000000
+#define MEM_SIZE  0x8000000
+#define MROM_BASE 0x20000000
 
 extern "C" void pmem_write(int waddr, int wdata, char wmask);
 extern "C" int pmem_read(int raddr);
@@ -13,5 +14,7 @@ extern "C" void mrom_read(int32_t addr, int32_t *data);
 void init_memory();
 uint8_t *guest2host(uint32_t paddr);
 uint32_t host2guest(uint8_t *haddr);
+uint8_t *guest2host_mrom(uint32_t paddr);
+uint32_t host2guest_mrom(uint8_t *haddr);
 
 #endif
