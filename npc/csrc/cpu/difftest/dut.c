a1a6939fe6da783ac58dcd35e0f57d380f5d04d2
tracer-ysyx <tracer@ysyx.org>
2024-07-02 10:38:25 +0800

    > sim RTL ysyx_23060236 王子卿 Linux WP 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:38:25 up 18:45,  5 users,  load average: 0.37, 0.29, 0.23

--- npc/csrc/cpu/difftest/dut.c
+++ npc/csrc/cpu/difftest/dut.c
@@ -52,7 +52,13 @@ static void checkregs(uint32_t *ref, uint32_t ref_pc, uint32_t pc) {
 	for (int i = 0; i < 32; ++i) {
 		if (ref[i] != top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__my_reg__DOT__rf[i])
 			trigger_difftest = 1;
-		if (pc != ref_pc) trigger_difftest = 1;
+	}
+	if (pc != ref_pc) trigger_difftest = 1;
+	if (trigger_difftest) {
+		printf("pc\t%#x\n", ref_pc);
+		for (int i = 0; i < 32; ++i) {
+			printf("x%d\t%#x\n", i, ref[i]);
+		}
 	}
 }
 
