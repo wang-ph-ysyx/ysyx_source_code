569e927d53b2ab3b6973e209711e22ce21380f37
tracer-ysyx <tracer@ysyx.org>
2024-07-03 16:34:44 +0800

    > sim RTL ysyx_23060236 王子卿 Linux WP 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:34:44 up  7:48,  9 users,  load average: 0.38, 0.32, 0.33

--- npc/csrc/memory/memory.c
+++ npc/csrc/memory/memory.c
@@ -19,6 +19,7 @@ void reg_display();
 
 extern "C" void flash_read(int32_t addr, int32_t *data) {
 	*data = *(int32_t *)(flash + addr);
+	printf("addr:%d, data:%d\n", addr, *data);
 }
 extern "C" void mrom_read(int32_t addr, int32_t *data) {
 	uint8_t *haddr = guest2host_mrom(addr);
