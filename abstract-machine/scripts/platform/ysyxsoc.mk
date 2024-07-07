b738b6928d34a9f99be0524bdda7e5d0bf80cc3c
tracer-ysyx <tracer@ysyx.org>
2024-07-07 17:22:08 +0800

    > sim RTL ysyx_23060236 王子卿 Linux WP 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:22:08 up  4:16,  5 users,  load average: 0.21, 0.34, 0.49

--- abstract-machine/scripts/platform/ysyxsoc.mk
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -12,7 +12,7 @@ CXXFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
 						 --defsym=_pmem_start=0x20000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
-CXXFLAGS += -DMAINARGS=\"$(mainargs)\"
+CXXFLAGS += -DMAINARGS=\"123$(mainargs)\"
 .PHONY: $(AM_HOME)/am/src/riscv/ysyxsoc/trm.c
 
 image: $(IMAGE).elf
