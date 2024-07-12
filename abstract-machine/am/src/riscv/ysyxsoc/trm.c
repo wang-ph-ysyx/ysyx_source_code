5e5257224e8964943407fcaa08460a61ee6d0f02
tracer-ysyx <tracer@ysyx.org>
2024-07-12 20:25:04 +0800

    > sim RTL ysyx_23060236 王子卿 Linux WP 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:25:04 up 11:31,  9 users,  load average: 0.63, 0.48, 0.46

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -20,7 +20,7 @@ static const char mainargs[] = MAINARGS;
 
 void putch(char ch) {
 	while (!(inb(SERIAL_PORT + 5) & 0x20));
-	for (volatile int i = 0; i < 100; ++i);
+	for (volatile int i = 0; i < 1000; ++i);
 	outb(SERIAL_PORT, ch);
 }
 
