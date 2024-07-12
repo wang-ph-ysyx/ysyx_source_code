0478af3384c23fda90a4ce288d33d9acdc78fb41
tracer-ysyx <tracer@ysyx.org>
2024-07-12 15:08:24 +0800

    > sim RTL ysyx_23060236 王子卿 Linux WP 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:08:24 up  6:15,  9 users,  load average: 0.29, 1.00, 2.19

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -35,6 +35,7 @@ extern char _bss_start [];
 extern char end[];
 
 void _trm_init() {
+	//set uart
 	uint8_t lcr = inb(SERIAL_PORT + 3);
 	outb(SERIAL_PORT + 3, 0x80 | lcr);
 	outb(SERIAL_PORT + 8, 0x01);
