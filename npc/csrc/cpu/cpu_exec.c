12f52b175083023400fe59975ae887665d415544
tracer-ysyx <tracer@ysyx.org>
2024-07-16 11:38:00 +0800

    > sim RTL ysyx_23060236 王子卿 Linux WP 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:38:00 up  2:51,  8 users,  load average: 0.24, 0.40, 0.49

--- npc/csrc/cpu/cpu_exec.c
+++ npc/csrc/cpu/cpu_exec.c
@@ -67,7 +67,7 @@ void cpu_exec(unsigned n) {
 		difftest = top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__wb_valid;
 #endif
 		if (inst == 0x100073 || trigger_difftest) break;
-		if (pc == 0xa0001910) break;
+		if (pc == 0xa0000e0c) break;
 	}
 
 	if (trigger_difftest) {
