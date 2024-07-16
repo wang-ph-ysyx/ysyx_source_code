b262d048b042d9810e946dec6b4e1bcc5f9c8f98
tracer-ysyx <tracer@ysyx.org>
2024-07-16 17:38:43 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:38:43 up  8:52, 10 users,  load average: 0.02, 0.08, 0.13

--- npc/vsrc/ysyx_23060236_lsu.v
+++ npc/vsrc/ysyx_23060236_lsu.v
@@ -1,46 +1,133 @@
 module ysyx_23060236_lsu(
 	input  clock,
-	input  [31:0] raddr,
-	input  ren,
-	output [31:0] val,
-	input  [31:0] waddr,
-	input  [31:0] wdata,
-	input  wen,
-	input  [7:0] wmask,
-	output valid,
-	input  [6:0] opcode,
-	input  [2:0] funct3
+	input  reset,
+
+	output [31:0] lsu_araddr,
+	output        lsu_arvalid,
+	output [2:0]  lsu_arsize,
+	input         lsu_arready,
+	input  [63:0] lsu_rdata,
+	input  [1:0]  lsu_rresp,
+	input         lsu_rvalid,
+	output        lsu_rready,
+
+	output [31:0] lsu_awaddr,
+	output        lsu_awvalid,
+	output [2:0]  lsu_arsize,
+	input         lsu_awready,
+	output [63:0] lsu_wdata,
+	output [7:0]  lsu_wstrb,
+	output        lsu_wvalid,
+	input         lsu_wready,
+	input  [1:0]  lsu_bresp,
+	input         lsu_bvalid,
+	output        lsu_bready
 );
 
-	reg state_load;
-	reg [31:0] rdata;
-
-	always @(posedge clock) begin
-		if (ren) begin
-			rdata <= pmem_read(raddr);
-			state_load <= 1;
-		end
-		else begin
-			state_load <= 0;
-			if (wen) begin
-				pmem_write(waddr, wdata, wmask);
-			end
-		end
-	end
-
-	assign valid = state_load;
-
-	ysyx_23060236_MuxKeyInternal #(5, 10, 32, 1) calculate_val(
-		.out(val),
+	ysyx_23060236_MuxKeyInternal #(5, 10, 3, 1) caculate_lsu_arsize(
+		.out(lsu_arsize),
 		.key({funct3, opcode}),
-		.default_out(32'b0),
+		.default_out(3'b0),
 		.lut({
-			10'b0000000011, (rdata & 32'hff) | {{24{rdata[7]}}, 8'h0},    //lb
-			10'b0010000011, (rdata& 32'hffff) | {{16{rdata[15]}}, 16'h0}, //lh
-			10'b0100000011, rdata,                                        //lw
-			10'b1000000011, rdata & 32'hff,                               //lbu
-			10'b1010000011, rdata & 32'hffff                              //lhu
+			10'b0000000011, 3'b000,   //lb
+			10'b0010000011, 3'b001,   //lh
+			10'b0100000011, 3'b010,   //lw
+			10'b1000000011, 3'b000,   //lbu
+			10'b1010000011, 3'b001    //lhu
 		})
 	);
 
+	ysyx_23060236_MuxKeyInternal #(3, 10, 3, 1) caculate_lsu_awsize(
+		.out(lsu_awsize),
+		.key({funct3, opcode}),
+		.default_out(3'b0),
+		.lut({
+			10'b0000100011, 3'b000,   //sb
+			10'b0010100011, 3'b001,   //sh
+			10'b0100100011, 3'b010    //sw
+		})
+	);
+
+	ysyx_23060236_MuxKeyInternal #(15, 12, 32, 1) caculate_lsu_val_tmp(
+		.out(lsu_val_tmp),
+		.key({lsu_aligned_64, lsu_aligned_32, funct3, opcode}),
+		.default_out(32'b0),
+		.lut({
+			12'b100000000011, (lsu_val_shift_64 & 32'hff) | {{24{lsu_val_shift_64[7]}}, 8'h0},      //lb
+			12'b100010000011, (lsu_val_shift_64 & 32'hffff) | {{16{lsu_val_shift_64[15]}}, 16'h0},  //lh
+			12'b100100000011, lsu_val_shift_64,                                                     //lw
+			12'b101000000011, lsu_val_shift_64 & 32'hff,                                            //lbu
+			12'b101010000011, lsu_val_shift_64 & 32'hffff,                                          //lhu
+			12'b010000000011, (lsu_val_shift_32 & 32'hff) | {{24{lsu_val_shift_32[7]}}, 8'h0},      //lb
+			12'b010010000011, (lsu_val_shift_32 & 32'hffff) | {{16{lsu_val_shift_32[15]}}, 16'h0},  //lh
+			12'b010100000011, lsu_val_shift_32,                                                     //lw
+			12'b011000000011, lsu_val_shift_32 & 32'hff,                                            //lbu
+			12'b011010000011, lsu_val_shift_32 & 32'hffff,                                          //lhu
+			12'b000000000011, (lsu_rdata[31:0] & 32'hff) | {{24{lsu_rdata[31:0][7]}}, 8'h0},        //lb
+			12'b000010000011, (lsu_rdata[31:0] & 32'hffff) | {{16{lsu_rdata[31:0][15]}}, 16'h0},    //lh
+			12'b000100000011, lsu_rdata[31:0],                                                      //lw
+			12'b001000000011, lsu_rdata[31:0] & 32'hff,                                             //lbu
+			12'b001010000011, lsu_rdata[31:0] & 32'hffff                                            //lhu
+		})
+	);
+
+	ysyx_23060236_MuxKeyInternal #(8, 3, 32, 1) caculate_lsu_val_shift_64(
+		.out(lsu_val_shift_64),
+		.key(lsu_araddr[2:0]),
+		.default_out(32'b0),
+		.lut({
+			3'b000, lsu_rdata[31:0],
+			3'b001, lsu_rdata[39:8],
+			3'b010, lsu_rdata[47:16],
+			3'b011, lsu_rdata[55:24],
+			3'b100, lsu_rdata[63:32],
+			3'b101, {8'b0, lsu_rdata[63:40]},
+			3'b110, {16'b0, lsu_rdata[63:48]},
+			3'b111, {24'b0, lsu_rdata[63:56]}
+		})
+	);
+
+	ysyx_23060236_MuxKeyInternal #(8, 3, 8, 1) calculate_lsu_val_shift_32(
+		.out(lsu_val_shift_32),
+		.key(lsu_araddr[1:0]),
+		.default_out(32'b0),
+		.lut({
+			2'b00, lsu_rdata[31:0],
+			2'b01, lsu_rdata[39:8],
+			2'b10, lsu_rdata[47:16],
+			2'b11, lsu_rdata[55:24]
+		})
+	);
+
+	ysyx_23060236_MuxKeyInternal #(8, 3, 8, 1) calculate_lsu_wstrb(
+		.out(lsu_wstrb),
+		.key(lsu_awaddr[2:0]),
+		.default_out(8'b0),
+		.lut({
+			3'b000, wmask,
+			3'b001, {wmask[6:0], 1'b0},
+			3'b010, {wmask[5:0], 2'b0},
+			3'b011, {wmask[4:0], 3'b0},
+			3'b100, {wmask[3:0], 4'b0},
+			3'b101, {wmask[2:0], 5'b0},
+			3'b110, {wmask[1:0], 6'b0},
+			3'b111, {wmask[0:0], 7'b0},
+		})
+	);
+
+	ysyx_23060236_MuxKeyInternal #(8, 3, 64, 1) calculate_lsu_wdata(
+		.out(lsu_wdata),
+		.key(lsu_awaddr[2:0]),
+		.default_out(64'b0),
+		.lut({
+			3'b000, {32'b0, src2},
+			3'b001, {24'b0, src2, 8'b0},
+			3'b010, {24'b0, src2, 8'b0},
+			3'b011, {24'b0, src2, 8'b0},
+			3'b100, {24'b0, src2, 8'b0},
+			3'b101, {24'b0, src2, 8'b0},
+			3'b110, {24'b0, src2, 8'b0},
+			3'b111, {24'b0, src2, 8'b0},
+		})
+	);
 endmodule
--- npc/vsrc/ysyx_23060236_top.v
+++ npc/vsrc/ysyx_23060236_top.v
@@ -291,111 +291,28 @@ module ysyx_23060236(
 		.wmask(wmask)
 	);
 
-	ysyx_23060236_MuxKeyInternal #(5, 10, 3, 1) caculate_lsu_arsize(
-		.out(lsu_arsize),
-		.key({funct3, opcode}),
-		.default_out(3'b0),
-		.lut({
-			10'b0000000011, 3'b000,   //lb
-			10'b0010000011, 3'b001,   //lh
-			10'b0100000011, 3'b010,   //lw
-			10'b1000000011, 3'b000,   //lbu
-			10'b1010000011, 3'b001    //lhu
-		})
-	);
-
-	ysyx_23060236_MuxKeyInternal #(3, 10, 3, 1) caculate_lsu_awsize(
-		.out(lsu_awsize),
-		.key({funct3, opcode}),
-		.default_out(3'b0),
-		.lut({
-			10'b0000100011, 3'b000,   //sb
-			10'b0010100011, 3'b001,   //sh
-			10'b0100100011, 3'b010    //sw
-		})
-	);
-
-	ysyx_23060236_MuxKeyInternal #(15, 12, 32, 1) caculate_lsu_val_tmp(
-		.out(lsu_val_tmp),
-		.key({lsu_aligned_64, lsu_aligned_32, funct3, opcode}),
-		.default_out(32'b0),
-		.lut({
-			12'b100000000011, (lsu_val_shift_64 & 32'hff) | {{24{lsu_val_shift_64[7]}}, 8'h0},      //lb
-			12'b100010000011, (lsu_val_shift_64 & 32'hffff) | {{16{lsu_val_shift_64[15]}}, 16'h0},  //lh
-			12'b100100000011, lsu_val_shift_64,                                                     //lw
-			12'b101000000011, lsu_val_shift_64 & 32'hff,                                            //lbu
-			12'b101010000011, lsu_val_shift_64 & 32'hffff,                                          //lhu
-			12'b010000000011, (lsu_val_shift_32 & 32'hff) | {{24{lsu_val_shift_32[7]}}, 8'h0},      //lb
-			12'b010010000011, (lsu_val_shift_32 & 32'hffff) | {{16{lsu_val_shift_32[15]}}, 16'h0},  //lh
-			12'b010100000011, lsu_val_shift_32,                                                     //lw
-			12'b011000000011, lsu_val_shift_32 & 32'hff,                                            //lbu
-			12'b011010000011, lsu_val_shift_32 & 32'hffff,                                          //lhu
-			12'b000000000011, (lsu_rdata[31:0] & 32'hff) | {{24{lsu_rdata[31:0][7]}}, 8'h0},        //lb
-			12'b000010000011, (lsu_rdata[31:0] & 32'hffff) | {{16{lsu_rdata[31:0][15]}}, 16'h0},    //lh
-			12'b000100000011, lsu_rdata[31:0],                                                      //lw
-			12'b001000000011, lsu_rdata[31:0] & 32'hff,                                             //lbu
-			12'b001010000011, lsu_rdata[31:0] & 32'hffff                                            //lhu
-		})
-	);
-
-	ysyx_23060236_MuxKeyInternal #(8, 3, 32, 1) caculate_lsu_val_shift_64(
-		.out(lsu_val_shift_64),
-		.key(lsu_araddr[2:0]),
-		.default_out(32'b0),
-		.lut({
-			3'b000, lsu_rdata[31:0],
-			3'b001, lsu_rdata[39:8],
-			3'b010, lsu_rdata[47:16],
-			3'b011, lsu_rdata[55:24],
-			3'b100, lsu_rdata[63:32],
-			3'b101, {8'b0, lsu_rdata[63:40]},
-			3'b110, {16'b0, lsu_rdata[63:48]},
-			3'b111, {24'b0, lsu_rdata[63:56]}
-		})
-	);
-
-	ysyx_23060236_MuxKeyInternal #(4, 2, 32, 1) caculate_lsu_val_shift_32(
-		.out(lsu_val_shift_32),
-		.key(lsu_araddr[1:0]),
-		.default_out(32'b0),
-		.lut({
-			2'b00, lsu_rdata[31:0],
-			2'b01, lsu_rdata[39:8],
-			2'b10, lsu_rdata[47:16],
-			2'b11, lsu_rdata[55:24]
-		})
-	);
-
-	ysyx_23060236_MuxKeyInternal #(8, 3, 8, 1) caculate_lsu_wstrb(
-		.out(lsu_wstrb),
-		.key(lsu_awaddr[2:0]),
-		.default_out(8'b0),
-		.lut({
-			3'b000, wmask,
-			3'b001, {wmask[6:0], 1'b0},
-			3'b010, {wmask[5:0], 2'b0},
-			3'b011, {wmask[4:0], 3'b0},
-			3'b100, {wmask[3:0], 4'b0},
-			3'b101, {wmask[2:0], 5'b0},
-			3'b110, {wmask[1:0], 6'b0},
-			3'b111, {wmask[0:0], 7'b0}
-		})
-	);
-
-	ysyx_23060236_MuxKeyInternal #(8, 3, 64, 1) calculate_lsu_wdata(
-		.out(lsu_wdata),
-		.key(lsu_awaddr[2:0]),
-		.default_out(64'b0),
-		.lut({
-			3'b000, {32'b0, src2},
-			3'b001, {24'b0, src2, 8'b0},
-			3'b010, {16'b0, src2, 16'b0},
-			3'b011, {8'b0, src2, 24'b0},
-			3'b100, {src2, 32'b0},
-			3'b101, {src2[23:0], 40'b0},
-			3'b110, {src2[15:0], 48'b0},
-			3'b111, {src2[7:0], 56'b0}
-		})
+	ysyx_23060236_lsu my_lsu(
+		.clock(clock),
+		.reset(reset),
+		.lsu_araddr(lsu_araddr),
+		.lsu_arvalid(lsu_arvalid),
+		.lsu_arready(lsu_arready),
+		.lsu_rdata(lsu_rdata),
+		.lsu_rresp(lsu_rresp),
+		.lsu_rvalid(lsu_rvalid),
+		.lsu_rready(lsu_rready),
+		.lsu_awaddr(lsu_awaddr),
+		.lsu_awvalid(lsu_awvalid),
+		.lsu_awready(lsu_awready),
+		.lsu_wdata(lsu_wdata),
+		.lsu_wstrb(lsu_wstrb),
+		.lsu_wvalid(lsu_wvalid),
+		.lsu_wready(lsu_wready),
+		.lsu_bresp(lsu_bresp),
+		.lsu_bvalid(lsu_bvalid),
+		.lsu_bready(lsu_bready),
+		.lsu_arsize(lsu_arsize),
+		.lsu_awsize(lsu_awsize),
 	);
 
 	ysyx_23060236_RegisterFile #(5, 32) my_reg(
