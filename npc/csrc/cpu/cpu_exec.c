007a76f0ea63f847512d6b91a29ed7681bd6b09c
tracer-ysyx <tracer@ysyx.org>
2024-07-21 09:43:41 +0800

    > sim RTL ysyx_23060236 王子卿 Linux WP 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:43:41 up 51 min,  8 users,  load average: 0.52, 0.36, 0.32

--- npc/csrc/cpu/cpu_exec.c
+++ npc/csrc/cpu/cpu_exec.c
@@ -23,8 +23,6 @@ static long total_lsu_readingcycle = 0;
 static long total_ifu_readingcycle = 0;
 static long hit_icache = 0;
 static long miss_icache = 0;
-static long icache_reading_cycle = 0;
-static long icache_writing_cycle = 0;
 static int lsu_awaddr = 0;
 
 enum { DIFFTEST_TO_DUT, DIFFTEST_TO_REF };
@@ -46,7 +44,6 @@ void print_statistic() {
 	printf("average lsu reading delay: %f\n", (double)total_lsu_readingcycle / total_lsu_getdata);
 	printf("hit_icache: %ld\nmiss_icache: %ld\n", hit_icache, miss_icache);
 	printf("hit rate: %f\n", (double)hit_icache / (hit_icache + miss_icache));
-	printf("icache_reading_cycle: %ld\nicache_writing_cycle: %ld\n", icache_reading_cycle, icache_writing_cycle);
 	printf("\n");
 }
 
@@ -58,8 +55,6 @@ extern "C" void add_lsu_readingcycle() { ++total_lsu_readingcycle; }
 extern "C" void add_ifu_readingcycle() { ++total_ifu_readingcycle; }
 extern "C" void add_hit_icache() { ++hit_icache; }
 extern "C" void add_miss_icache() { ++miss_icache; }
-extern "C" void add_icache_reading_cycle() { ++icache_reading_cycle; }
-extern "C" void add_icache_writing_cycle() { ++icache_writing_cycle; }
 extern "C" void record_lsu_awaddr(int awaddr) { lsu_awaddr = awaddr; }
 
 static void one_cycle() {
