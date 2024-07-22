9afe80151ab716c9077c7ccd2157557960161a72
tracer-ysyx <tracer@ysyx.org>
2024-07-22 19:28:37 +0800

    > sim RTL ysyx_23060236 王子卿 Linux WP 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:28:37 up 10:21, 10 users,  load average: 0.82, 0.41, 0.24

--- npc/vsrc/ysyx_23060236_CSRFile.v
+++ npc/vsrc/ysyx_23060236_CSRFile.v
@@ -13,23 +13,45 @@ module ysyx_23060236_CSRFile #(DATA_WIDTH = 1) (
 	input valid
 );
 
-	wire [31:0] rdata_tmp;
-	reg [DATA_WIDTH-1:0] mepc;
-	reg [DATA_WIDTH-1:0] mcause;
-	reg [DATA_WIDTH-1:0] mstatus;
-	reg [DATA_WIDTH-1:0] mtvec;
+	localparam ADDR_MEPC      = 3'd0;
+	localparam ADDR_MCAUSE    = 3'd1;
+	localparam ADDR_MSTATUS   = 3'd2;
+	localparam ADDR_MTVEC     = 3'd3;
+	localparam ADDR_MVENDORID = 3'd4;
+	localparam ADDR_MARCHID   = 3'd5;
 
-	ysyx_23060236_MuxKeyInternal #(6, 12, 32, 1) calculate_rdata_tmp(
-		.out(rdata_tmp),
+	wire [31:0] rdata_tmp;
+	wire [2:0]  addr;
+	reg  [DATA_WIDTH-1:0] mepc;
+	reg  [DATA_WIDTH-1:0] mcause;
+	reg  [DATA_WIDTH-1:0] mstatus;
+	reg  [DATA_WIDTH-1:0] mtvec;
+
+	ysyx_23060236_MuxKeyInternal #(6, 12, 3, 1) calculate_addr(
+		.out(addr),
 		.key(imm),
+		.default_out(3'b0),
+		.lut({
+			12'h341, ADDR_MEPC,
+      12'h342, ADDR_MCAUSE,    
+      12'h300, ADDR_MSTATUS,   
+      12'h305, ADDR_MTVEC,
+      12'hf11, ADDR_MVENDORID,
+      12'hf12, ADDR_MARCHID   
+		})
+	);
+
+	ysyx_23060236_MuxKeyInternal #(6, 3, 32, 1) calculate_rdata_tmp(
+		.out(rdata_tmp),
+		.key(addr),
 		.default_out(32'b0),
 		.lut({
-			12'h341, mepc,
-			12'h342, mcause,
-			12'h300, mstatus,
-			12'h305, mtvec,
-			12'hf11, 32'h79737978,
-			12'hf12, 32'h015fdf0c
+			ADDR_MEPC     , mepc,
+			ADDR_MCAUSE   , mcause,
+			ADDR_MSTATUS  , mstatus,
+			ADDR_MTVEC    , mtvec,
+			ADDR_MVENDORID, 32'h79737978,
+			ADDR_MARCHID  , 32'h015fdf0c
 		})
 	);
 
@@ -54,7 +76,7 @@ module ysyx_23060236_CSRFile #(DATA_WIDTH = 1) (
 		end
 	end
 
-	assign rdata = enable ? rdata_tmp : 32'b0;
+	assign rdata = {32{enable}} & rdata_tmp;
 	assign jump = {32{inst_ecall}} & mtvec | {32{inst_mret}} & mepc;
 
 endmodule
