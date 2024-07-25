2127372d0f615ea9e3e0da262f56e601b786e1ce
tracer-ysyx <tracer@ysyx.org>
2024-07-25 18:51:26 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:51:26 up  9:43,  8 users,  load average: 3.46, 2.71, 1.78

--- npc/vsrc/ysyx_23060236_top.v
+++ npc/vsrc/ysyx_23060236_top.v
@@ -447,7 +447,7 @@ import "DPI-C" function void add_ifu_getinst();
 
 	always @(posedge clock) begin
 		add_total_cycle();
-		if (ifu_rvalid & ifu_rready) add_ifu_getinst();
+		if (ifu_rvalid & ifu_rready | icache_rvalid & icache_rready & ~icache_rresp[1]) add_ifu_getinst();
 		if (wb_valid) add_total_inst();
 		if (lsu_rvalid & lsu_rready) add_lsu_getdata();
 	end
