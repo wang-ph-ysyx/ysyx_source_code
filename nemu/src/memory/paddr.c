cb53e1dd369809949d841e842cdf131ce8235d1d
tracer-ysyx <tracer@ysyx.org>
2024-07-26 16:16:23 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:16:23 up  7:14,  8 users,  load average: 0.64, 0.37, 0.25

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -89,6 +89,9 @@ word_t paddr_read(paddr_t addr, int len) {
 
 void paddr_write(paddr_t addr, int len, word_t data) {
 	IFDEF(CONFIG_MTRACE, mtrace_write(addr, len, data));
+#ifdef CTRACE
+	if (addr >= 0x10000000 && addr < 0x10001000) return;
+#endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
