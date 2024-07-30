749af0ffe42e1019ce981f11df1cc93671a8c005
tracer-ysyx <tracer@ysyx.org>
2024-07-30 16:23:54 +0800

    > sim RTL ysyx_23060236 王子卿 Linux WP 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:23:54 up  7:21, 10 users,  load average: 0.56, 0.32, 0.31

--- npc/vsrc/ysyx_23060236_CSRFile.v
+++ npc/vsrc/ysyx_23060236_CSRFile.v
@@ -6,50 +6,59 @@ module ysyx_23060236_CSRFile #(DATA_WIDTH = 32) (
 	output [DATA_WIDTH-1:0] rdata,
 	input  enable,
 	input  inst_ecall,
+	input  inst_mret,
 	input  [31:0] epc,
 	output [31:0] jump,
-	input  inst_mret,
 	input  valid
 );
 
-	wire [2:0] addr;
-	reg [DATA_WIDTH-1:0] csr [5:0];
+	wire [5:0] choose;
+	reg [DATA_WIDTH-1:0] mepc   ;
+	reg [DATA_WIDTH-1:0] mcause ;
+	reg [DATA_WIDTH-1:0] mstatus;
+	reg [DATA_WIDTH-1:0] mtvec  ;
 
-	localparam ADDR_MEPC      = 3'd0;
-	localparam ADDR_MCAUSE    = 3'd1;
-	localparam ADDR_MSTATUS   = 3'd2;
-	localparam ADDR_MTVEC     = 3'd3;
-	localparam ADDR_MVENDORID = 3'd4;
-	localparam ADDR_MARCHID   = 3'd5;
+	localparam MEPC      = 0;
+	localparam MCAUSE    = 1;
+	localparam MSTATUS   = 2;
+	localparam MTVEC     = 3;
+	localparam MVENDORID = 4;
+	localparam MARCHID   = 5;
 
-	assign addr = (imm == 12'h341) ? ADDR_MEPC      :
-	              (imm == 12'h342) ? ADDR_MCAUSE    :
-	              (imm == 12'h300) ? ADDR_MSTATUS   :
-	              (imm == 12'h305) ? ADDR_MTVEC     :
-	              (imm == 12'hf11) ? ADDR_MVENDORID :
-	              (imm == 12'hf12) ? ADDR_MARCHID   :
-								3'b000;
+	assign choose[MEPC     ] = (imm == 12'h341);
+	assign choose[MCAUSE   ] = (imm == 12'h342);
+	assign choose[MSTATUS  ] = (imm == 12'h300);
+	assign choose[MTVEC    ] = (imm == 12'h305);
+	assign choose[MVENDORID] = (imm == 12'hf11);
+	assign choose[MARCHID  ] = (imm == 12'hf12);
 
 	always @(posedge clock) begin
 		if (reset) begin
-			csr[ADDR_MSTATUS]   <= 32'h1800;
-			csr[ADDR_MVENDORID] <= 32'h79737978;
-			csr[ADDR_MARCHID]   <= 32'h015fdf0c;
+			mstatus <= 32'h1800;
 		end
 		else if (valid) begin
 			if (enable) begin
-				csr[addr] <= wdata;
-				csr[ADDR_MVENDORID] <= 32'h79737978;
-				csr[ADDR_MARCHID]   <= 32'h015fdf0c;
+				if (choose[MEPC   ]) mepc    <= wdata;
+				if (choose[MCAUSE ]) mcause  <= wdata;
+				if (choose[MSTATUS]) mstatus <= wdata;
+				if (choose[MTVEC  ]) mtvec   <= wdata;
 			end
 			else if (inst_ecall) begin
-				csr[ADDR_MEPC]   <= epc;
-				csr[ADDR_MCAUSE] <= 32'd11;
+				mepc   <= epc;
+				mcause <= 32'd11;
 			end
 		end
 	end
 
-	assign rdata = {32{enable}} & csr[addr];
-	assign jump = {32{inst_ecall}} & csr[ADDR_MTVEC] | {32{inst_mret}} & csr[ADDR_MEPC];
+	assign rdata = ~enable ? 32'b0 : 
+								 choose[MEPC     ] ? mepc    :
+                 choose[MCAUSE   ] ? mcause  :
+                 choose[MSTATUS  ] ? mstatus :
+                 choose[MTVEC    ] ? mtvec   :
+                 choose[MVENDORID] ? 32'h79737978 :
+                 choose[MARCHID  ] ? 32'h015fdf0c :
+								 32'b0;
+
+	assign jump = {32{inst_ecall}} & mtvec | {32{inst_mret}} & mepc;
 
 endmodule
