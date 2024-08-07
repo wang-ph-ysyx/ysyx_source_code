92e6d9ba893870b9f6a7ea680479da0d72d14c06
tracer-ysyx <tracer@ysyx.org>
2024-08-07 20:20:22 +0800

    > sim RTL ysyx_23060236 王子卿 Linux WP 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:20:22 up  7:38, 12 users,  load average: 0.78, 0.53, 0.45

--- npc/vsrc/ysyx_23060236_RegisterFile.v
+++ npc/vsrc/ysyx_23060236_RegisterFile.v
@@ -11,7 +11,24 @@ module ysyx_23060236_RegisterFile #(ADDR_WIDTH = 4, DATA_WIDTH = 32) (
 	input valid
 );
 
-  reg [DATA_WIDTH-1:0] rf [2**ADDR_WIDTH-1:1];
+  reg  [DATA_WIDTH-1:0] rf [2**ADDR_WIDTH-1:1];
+  wire [DATA_WIDTH-1:0] rf_read [2**ADDR_WIDTH-1:0];
+	assign rf_read[0]  = 0;
+	assign rf_read[1]  = rf[1];
+	assign rf_read[2]  = rf[2];
+	assign rf_read[3]  = rf[3];
+	assign rf_read[4]  = rf[4];
+	assign rf_read[5]  = rf[5];
+	assign rf_read[6]  = rf[6];
+	assign rf_read[7]  = rf[7];
+	assign rf_read[8]  = rf[8];
+	assign rf_read[9]  = rf[9];
+	assign rf_read[10] = rf[10];
+	assign rf_read[11] = rf[11];
+	assign rf_read[12] = rf[12];
+	assign rf_read[13] = rf[13];
+	assign rf_read[14] = rf[14];
+	assign rf_read[15] = rf[15];
 
   always @(posedge clock) begin
 		if (valid & wen & (waddr != 0)) begin
@@ -19,7 +36,7 @@ module ysyx_23060236_RegisterFile #(ADDR_WIDTH = 4, DATA_WIDTH = 32) (
 		end
   end
 
-	assign rdata1 = (raddr1 == 0) ? 0 : rf[raddr1];
-	assign rdata2 = (raddr2 == 0) ? 0 : rf[raddr2];
+	assign rdata1 = rf_read[raddr1];
+	assign rdata2 = rf_read[raddr2];
 
 endmodule
