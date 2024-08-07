8f0541db5c4cb73551f9e4780175e27be79c9202
tracer-ysyx <tracer@ysyx.org>
2024-08-07 18:25:29 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:25:29 up  5:43,  8 users,  load average: 0.14, 0.13, 0.13

--- npc/csrc/cpu/cpu_exec.c
+++ npc/csrc/cpu/cpu_exec.c
@@ -97,7 +97,7 @@ void cpu_exec(unsigned long n) {
 		static int difftest = 0;
 		bool in_uart = (lsu_awaddr >= UART_BASE) && (lsu_awaddr < UART_BASE + UART_SIZE);
 		bool in_clint = (lsu_awaddr >= CLINT_BASE) && (lsu_awaddr < CLINT_BASE + CLINT_SIZE);
-		if ((((inst & 0x7f) == 0x23) || ((inst & 0x7f) == 0x03)) && (in_uart || in_clint)) {
+		if ((top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__lsu_awvalid || top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__lsu_arvalid) && (in_uart || in_clint)) {
 			difftest_skip_ref();
 		}
 		if (difftest)
