de920fd316bfa960133c64e24733130623ae568c
tracer-ysyx <tracer@ysyx.org>
2024-08-08 21:03:53 +0800

    > sim RTL ysyx_23060236 王子卿 Linux WP 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:03:53 up 11:54,  8 users,  load average: 0.45, 0.30, 0.32

--- npc/csrc/cpu/difftest/dut.c
+++ npc/csrc/cpu/difftest/dut.c
@@ -49,7 +49,7 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
 }
 
 static void checkregs(uint32_t *ref, uint32_t ref_pc, uint32_t pc) {
-	for (int i = 1; i < 16; ++i) {
+	for (int i = 0; i < 15; ++i) {
 		if (ref[i] != top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__my_reg__DOT__rf[i])
 			trigger_difftest = 1;
 	}
