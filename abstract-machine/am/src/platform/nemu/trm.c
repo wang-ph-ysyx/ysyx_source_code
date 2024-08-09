b7e38a4ab32ea6da0053d645bc4b0a5b0348fc69
tracer-ysyx <tracer@ysyx.org>
2024-08-09 17:49:16 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:49:16 up  8:22,  9 users,  load average: 0.40, 0.39, 0.37

--- abstract-machine/am/src/platform/nemu/trm.c
+++ abstract-machine/am/src/platform/nemu/trm.c
@@ -4,7 +4,7 @@
 extern char _heap_start;
 int main(const char *args);
 
-Area heap = RANGE(&_heap_start, &_heap_start);
+Area heap = RANGE(&_heap_start, PMEM_END);
 #ifndef MAINARGS
 #define MAINARGS ""
 #endif
