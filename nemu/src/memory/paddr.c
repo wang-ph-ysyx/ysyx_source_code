9058a3e3950913c948c0235f0a3ebabd52ea7b38
tracer-ysyx <tracer@ysyx.org>
2024-08-09 17:42:12 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:42:12 up  8:15,  9 users,  load average: 0.34, 0.33, 0.36

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -81,7 +81,7 @@ void init_mem() {
 
 word_t paddr_read(paddr_t addr, int len) {
 	IFDEF(CONFIG_MTRACE, mtrace_read(addr, len));
-#ifdef CONFIG_CTRACE
+/*#ifdef CONFIG_CTRACE
 	if (addr >= 0x10000000 && addr < 0x10001000) return 0x23;
 	if (addr >= 0x02000000 && addr < 0x02010000) return 0x0;
 #else
@@ -89,7 +89,7 @@ word_t paddr_read(paddr_t addr, int len) {
 	if (addr >= 0x10000000 && addr < 0x10001000) return 0x23;
 	if (addr >= 0x02000000 && addr < 0x02010000) return 0x0;
 #endif
-#endif
+#endif*/
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
@@ -98,13 +98,13 @@ word_t paddr_read(paddr_t addr, int len) {
 
 void paddr_write(paddr_t addr, int len, word_t data) {
 	IFDEF(CONFIG_MTRACE, mtrace_write(addr, len, data));
-#ifdef CONFIG_CTRACE
+/*#ifdef CONFIG_CTRACE
 	if (addr >= 0x10000000 && addr < 0x10001000) return;
 #else
 #ifdef CONFIG_BTRACE
 	if (addr >= 0x10000000 && addr < 0x10001000) return;
 #endif
-#endif
+#endif*/
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
