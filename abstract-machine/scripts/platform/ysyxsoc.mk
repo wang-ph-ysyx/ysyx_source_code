a9f461262e080ef9af74eeb0f5f3ed9f290c43b8
tracer-ysyx <tracer@ysyx.org>
2024-08-09 17:14:29 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:14:29 up  7:47,  9 users,  load average: 2.33, 0.75, 0.37

--- abstract-machine/scripts/platform/ysyxsoc.mk
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -18,7 +18,7 @@ CFLAGS += -I$(AM_HOME)/am/src/riscv/ysyxsoc/include
 CACHETRACE = $(WORK_DIR)/build/icache_trace.bin
 CACHESIMFLAGS = -b -c $(CACHETRACE)
 CACHESIM_HOME = /home/ysyx/cachesim
-BRANCHTRACE = $(WORKDIR)/build/branch_trace.bin
+BRANCHTRACE = $(WORK_DIR)/build/branch_trace.bin
 BRANCHSIMFLAGS = -b -r $(BRANCHTRACE)
 BRANCHSIM_HOME = /home/ysyx/branchsim
 .PHONY: $(AM_HOME)/am/src/riscv/ysyxsoc/trm.c
