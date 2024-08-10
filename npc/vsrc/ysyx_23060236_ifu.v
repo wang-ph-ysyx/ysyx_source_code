aa02900737ac1756995e65f11b0baae1a72f2cea
tracer-ysyx <tracer@ysyx.org>
2024-08-10 16:51:17 +0800

    > sim RTL ysyx_23060236 王子卿 Linux WP 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:51:17 up  7:16,  8 users,  load average: 0.64, 0.55, 0.62

--- npc/vsrc/ysyx_23060236_ifu.v
+++ npc/vsrc/ysyx_23060236_ifu.v
@@ -58,7 +58,8 @@ module ysyx_23060236_ifu(
 										inst;
 	assign ifu_over = (icache_rvalid & icache_hit | icache_wvalid & last | ifu_rvalid & ifu_rready & ~pc_in_sdram);
 	assign ifu_valid = idu_valid & idu_ready | (jump_wrong | jump_wrong_state) & (idu_valid | ifu_over);
-	assign icache_awaddr_tmp = (icache_rvalid & ~icache_hit) ? (pc & ~32'hf) : 
+	//与icache的块大小一致
+	assign icache_awaddr_tmp = (icache_rvalid & ~icache_hit) ? (pc & ~32'h1f) : 
 														 (icache_wvalid & ~last) ? (icache_awaddr + 4) : 
 														 icache_awaddr;
 	assign pc_tmp = ((jump_wrong | jump_wrong_state) & (idu_valid | ifu_over)) ? jump_addr : 
