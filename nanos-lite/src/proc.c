92cb6ebed6f22c9a027ad15d98a1051402e32a1d
tracer-ysyx <tracer@ysyx.org>
2024-10-27 16:56:50 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:56:50 up  5:38,  5 users,  load average: 0.41, 0.54, 0.70

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -42,7 +42,7 @@ void init_proc() {
 
 Context* schedule(Context *prev) {
 	current->cp = prev;
-	if (current == &pcb[0]) current = &pcb[1];
-	else current = &pcb[0];
+	if (current == &pcb[1]) current = &pcb[0];
+	else current = &pcb[1];
   return current->cp;
 }
