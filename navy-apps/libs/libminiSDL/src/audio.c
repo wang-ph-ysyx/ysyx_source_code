771e03f3a6388fe76676c92ac361b89850a1b1f1
tracer-ysyx <tracer@ysyx.org>
2024-10-28 21:14:37 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:14:37 up 11:45,  5 users,  load average: 4.66, 4.97, 5.04

--- navy-apps/libs/libminiSDL/src/audio.c
+++ navy-apps/libs/libminiSDL/src/audio.c
@@ -51,17 +51,21 @@ void SDL_UnlockAudio() {
 
 void CallBackHelper() {
 	static int called = 0;
+	printf("call callbackhelper: %d, %d, %d\n", time_interval, audio_pause, called);
 	if (time_interval == 0 || audio_pause || called) return;
 	called = 1;
 
+	printf("test1\n");
 	static uint32_t start = 0;
 	uint32_t now = SDL_GetTicks();
+	printf("test2\n");
 	if (now - start > time_interval && NDL_QueryAudio() > samples) {
 		start = now;
 		uint8_t *stream = malloc(samples);
-	printf("call callbackhelper: %d, %d, %d\n", time_interval, audio_pause, called);
+	printf("test3\n");
 		callback(NULL, stream, samples);
 		NDL_PlayAudio(stream, samples);
+	printf("test4\n");
 		free(stream);
 	}
 
