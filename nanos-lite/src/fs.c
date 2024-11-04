d358fa62477372f3ee0cf847ffbaebc910b6f344
tracer-ysyx <tracer@ysyx.org>
2024-11-04 21:55:37 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:55:37 up  9:46,  3 users,  load average: 0.27, 0.37, 0.43

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -54,6 +54,7 @@ void init_fs() {
 }
 
 int fs_open(const char *pathname, int flags, int mode) {
+	printf("open %s\n", pathname);
 	for (int i = 0; i < sizeof(file_table) / sizeof(Finfo); ++i) {
 		if (strcmp(file_table[i].name, pathname) == 0) {
 			file_table[i].open_offset = 0;
