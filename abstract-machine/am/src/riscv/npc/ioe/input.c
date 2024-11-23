f7ec967dc1e24a8b9c7e69e6254606461ad992c0
tracer-ysyx <tracer@ysyx.org>
2024-11-24 00:32:06 +0800

    > sim RTL ysyx_23060236 王子卿 Linux WP 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:32:06 up 13:25, 10 users,  load average: 4.79, 4.97, 4.82

--- abstract-machine/am/src/riscv/npc/ioe/input.c
+++ abstract-machine/am/src/riscv/npc/ioe/input.c
@@ -28,7 +28,7 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
 	}
 
 	kbd->keycode |= scancode;
-	printf("%x\n", kbd->keycode);
+	printf("%d %x\n", kbd->keydown, kbd->keycode);
 	return;
 	kbd->keycode = scan_to_keycode(kbd->keycode);
 }
