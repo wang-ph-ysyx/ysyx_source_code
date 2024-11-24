9d72e5bbed5c281529a2ea814fcfb2a55ec61cfd
tracer-ysyx <tracer@ysyx.org>
2024-11-24 22:53:35 +0800

    > sim RTL ysyx_23060236 王子卿 Linux WP 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:53:35 up 12:16,  7 users,  load average: 0.21, 0.32, 0.27

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,6 +15,7 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
+	yield();
 	for (int i = 0; i < len; ++i) {
 		putch(*((uint8_t *)buf + i));
 	}
