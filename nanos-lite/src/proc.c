81ffd2a8ff05f9d6b74768985f7a55892592953e
tracer-ysyx <tracer@ysyx.org>
2024-11-24 23:11:51 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:11:51 up 12:35,  1 user,  load average: 0.43, 0.34, 0.28

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -32,15 +32,15 @@ void init_proc() {
 	char *argv0[] = {"/bin/hello", NULL}, *envp0[] = {NULL};
 	context_uload(&pcb[0], "/bin/hello", argv0, envp0);
 
-	char *argv1[] = {"/bin/hello", NULL}, *envp1[] = {NULL};
-	context_uload(&pcb[1], "/bin/hello", argv1, envp1);
-/*
+	char *argv1[] = {"/bin/bird", NULL}, *envp1[] = {NULL};
+	context_uload(&pcb[1], "/bin/bird", argv1, envp1);
+
 	char *argv2[] = {"/bin/pal", "--skip", NULL}, *envp2[] = {NULL};
 	context_uload(&pcb[2], "/bin/pal", argv2, envp2);
 
 	char *argv3[] = {"/bin/nslider", NULL}, *envp3[] = {NULL};
 	context_uload(&pcb[3], "/bin/nslider", argv3, envp3);
-*/
+
   switch_boot_pcb();
 
   Log("Initializing processes...");
@@ -53,10 +53,17 @@ void init_proc() {
 Context* schedule(Context *prev) {
 	current->cp = prev;
 
-	if (current == &pcb[0]) {
-		current = &pcb[1];
+	static int count = 0;
+	if (current != &pcb[fg_pcb]) {
+		current = &pcb[fg_pcb];
 	}
-	else current = &pcb[0];
+	else {
+		if (count == 100) {
+			current = &pcb[0];
+			count = 0;
+		}
+		++count;
+	}
 
   return current->cp;
 }
