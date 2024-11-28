00e82890a2f6ad0dfe01a148e678023be3f6f83b
tracer-ysyx <tracer@ysyx.org>
2024-11-28 16:52:10 +0800

    > compile NEMU ysyx_23060236 王子卿 Linux WP 6.8.0-49-generic #49~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Nov  6 17:42:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:52:10 up  7:03,  5 users,  load average: 5.22, 5.00, 4.80

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -5,9 +5,9 @@
 #include <proc.h>
 
 #define CHANGE_PROC(a1, a2, a3) \
-	current->max_brk = 0; \
+{ current->max_brk = 0; \
 	context_uload(current, a1, a2, a3); \
-	switch_boot_pcb(); yield();
+	switch_boot_pcb(); yield(); }
 
 int fs_open(const char *pathname, int flags, int mode);
 size_t fs_read(int fd, void *buf, size_t len);
@@ -47,11 +47,8 @@ void do_syscall(Context *c) {
 		case SYS_lseek: c->GPRx = fs_lseek(a[1], a[2], a[3]); break;
 		case SYS_brk:   c->GPRx = mm_brk(a[1]); break;
 		case SYS_execve:if (fs_open((char *)a[1], 0, 0) < 0) c->GPRx = -2;
-										else { 
-											current->max_brk = 0;
-											context_uload(current, (char *)a[1], (char **)a[2], (char **)a[3]);
-										  switch_boot_pcb(); yield(); 
-										} break;
+										else { CHANGE_PROC((char *)a[1], (char **)a[2], (char **)a[3]) }
+										break;
 		case SYS_gettimeofday: c->GPRx = sys_gettimeofday((struct timeval *)a[1], (struct timezone *)a[2]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
