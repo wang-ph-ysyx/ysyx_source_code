0cb944527470efc7117cf0120519a70305f52e68
tracer-ysyx <tracer@ysyx.org>
2025-03-22 18:47:50 +0800

    > sim RTL ysyx_23060236 王子卿 Linux WP 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:47:50 up  6:28,  5 users,  load average: 0.24, 0.15, 0.22

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -34,13 +34,14 @@ extern char end[];
 
 void _trm_init() {
 	//set uart divisor
+/*
 	uint8_t lcr = inb(SERIAL_PORT + 3);
 	outb(SERIAL_PORT + 3, 0x80 | lcr);
 	outb(SERIAL_PORT, 0x01);
 	outb(SERIAL_PORT + 3, 0x7f & lcr);
 	//memcpy(_data_start, data_load_start, (size_t) (_bss_start - _data_start));
 	//memset(_bss_start, 0, (size_t)(end - _bss_start));
-/*
+
 	uint32_t ysyx = _read_csr_mvendorid(), ID = _read_csr_marchid();
 	char ysyx_s[4];
 	for (int i = 3; i >= 0; --i) {
