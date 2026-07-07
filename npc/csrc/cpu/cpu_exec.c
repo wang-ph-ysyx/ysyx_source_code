d8a0f34388ef05ba312b937e7777112cb0b22a9c
王子卿 <wang_ph@sjtu.edu.cn>
2026-07-07 15:59:06 +0800

    波形文件格式改为fst,合并链接脚本中的extra段以跑通riscv-arch-test

--- npc/csrc/cpu/cpu_exec.c
+++ npc/csrc/cpu/cpu_exec.c
@@ -5,7 +5,7 @@
 #include <memory.h>
 #include <config.h>
 #include <nvboard.h>
-#include "verilated_vcd_c.h"
+#include "verilated_fst_c.h"
 
 #if defined(__PLATFORM_ysyxsoc_)
 #include <VysyxSoCFull___024root.h>
@@ -18,7 +18,7 @@
 #endif
 
 TOP_NAME *top = NULL;
-VerilatedVcdC *tfp = NULL;
+VerilatedFstC *tfp = NULL;
 VerilatedContext *contextp = NULL;
 int trigger_difftest = 0;
 extern int wave_trace;
